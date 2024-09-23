8c7e517ed1fa1cbd52888b3e77116eb317ee5070
Oliver Gustafsson <oliver.gustafsson17@gmail.com>
2024-09-23 03:52:37 +0200

    welcome message now displays figlet

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,29 @@
 
 import { getCommands } from './helpers/command.helper'
 import Logger from './classes/logger.class'
+import figlet from 'figlet'
 
 const [, , ...args] = process.argv
 
+async function displayFiglet() {
+  return new Promise<void>((resolve, reject) => {
+    figlet('Ompa', (err, data) => {
+      if (err) {
+        Logger.error('Failed to create figlet')
+        return reject(err)
+      }
+
+      console.log(data)
+      Logger.newline()
+      return resolve()
+    })
+  })
+}
+
 async function welcome() {
   const commands = await getCommands()
+
+  await displayFiglet()
 
   Logger.info('Thanks for using Ompa')
   Logger.info('Here are some subcommands you may use')
