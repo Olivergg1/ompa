64bef8a1f5f22f7b745ea0e6a2fefebe649674e0
Oliver Gustafsson <oliver.gustafsson17@gmail.com>
2024-09-26 01:22:06 +0200

    add comments

--- src/scripts/loadResources.ts
+++ src/scripts/loadResources.ts
@@ -5,25 +5,32 @@ import Logger from '../classes/logger.class'
 export async function loadResources(force: boolean = false) {
   Logger.newline().info('Loading resources...')
 
+  // Warn if --force flag is set
   if (force) Logger.warning('Force flag set, overwriting exisiting resources')
 
+  // Find path to resources
   const resourcesPath = path.resolve('src', 'resources')
 
+  // Find path to default resources directory
   const defaultResourcesPath = path.resolve(resourcesPath, 'default')
 
+  // Filter out any non-resources
   const defaultResources = fs
     .readdirSync(defaultResourcesPath, {
       withFileTypes: true,
     })
     .filter(f => f.name.endsWith('resource.json'))
 
+  // Copy default resource into resource directory if it not already exists
   for (let res of defaultResources) {
     const resourcePath = path.resolve(resourcesPath, res.name)
 
+    // If resource alread exists and force flag is not set, skip without copying
     if (!force && fs.existsSync(resourcePath)) continue
 
     Logger.cyan('Creating resource', res.name)
 
+    // Move a copy of default resource into resource directory
     const defaultResourcePath = path.resolve(defaultResourcesPath, res.name)
 
     fs.copyFileSync(defaultResourcePath, resourcePath)
